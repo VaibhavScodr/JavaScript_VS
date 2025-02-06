8b7e09e118f5cbfa9db22fe44ee8acdb6c756c3e
VaibhavScodr <cs2021183@gobal.org.in>
2025-02-06 10:57:55 +0530

    'done

--- datatypes.js
+++ datatypes.js
@@ -1,5 +1,21 @@
 let namee="vaibhav"
 const id="55qmngh"
 namee='manav';
+val=2;
 var pass="alpha@123";
-console.table([namee,id,pass]);
+console.table([namee,id,pass,val]);
+//allert
+//alert(4+5); // this will run only on the web browser not in vs code
+//ECMA script is the standrd of the javascript and latest is ECMA6
+//datatype in js 
+/*1. number
+2. string
+3. symbol
+4. boolean
+5. null --> let state=null; is a value 
+6. undefined let state; <--undefined
+7. BigInt()
+*/
+console.log(typeof val);
+//type of null
+console.log(typeof null)  //is object .
