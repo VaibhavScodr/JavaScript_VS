a5c9010ef946d42a84a5bcdf74e9db4c1932cd33
VaibhavScodr <cs2021183@gobal.org.in>
2025-02-08 14:05:48 +0530

    sone

--- comparison.js
+++ comparison.js
@@ -15,4 +15,20 @@ console.log('2'==1)
 /*
    there are two types of memory like stack memory and heap memory
    Stack(Primitive)<--call by referance  Heap(Non-Primitive)<--call by referance
+
 */
+
+let x="we";
+let y="are";
+let z=new String("vaibhav");
+console.log(`vowels are ${x} ${y}`); //<-- Advance concatination
+
+console.log(z.charAt(0));
+console.log(x.length);
+console.log(x.toUpperCase())
+console.log(z.toLocaleUpperCase())
+console.log(z.indexOf('v'));
+let string="leetcode";
+console.log(string.charAt(0));
+
+// minus index's starts from last -1 -2 -3 -4 -5 
